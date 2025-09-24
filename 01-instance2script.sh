1689eb0d18d317c22bba93c4d1bdabe94975c4d1
azharmd-dev <azharmd0794@gmail.com>
2025-09-24 16:49:50 +0530

    To create instance through script

---
+++ 01-instance2script.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+AMI_ID="ami-09c813fb71547fc4f" #Amazon Machine ID
+SG_ID="sg-06e98ee3d4e95cf41" #Security Group ID
+ZONE_ID="Z0553382WPE1E3VPCYS6" #ZoneID from Route53
+DOMAIN_NAME="azharprojects.site"
+
+for instance in $@ # mongodb redis mysql
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+
+    # To get Private IP
+    if [ $instance != "frontend" ]; then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.azharprojects.site
+    else
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        RECORD_NAME="$DOMAIN_NAME" # azharprojects.site
+    fi
+
+    echo "$instance: $IP"
+
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$RECORD_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP'"
+            }]
+        }
+        }]
+    }
+    '
+done
